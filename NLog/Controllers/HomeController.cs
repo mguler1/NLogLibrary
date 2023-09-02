265a2ee78fa3c7ac1f4510d464621c1c139223de
Muhammed Mehti GÜLER <mehti.guler@gmail.com>
2023-09-03 00:48:14 +0300

    textfile loglama yapıldı

---
+++ NLog/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NLogLibrary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog injected into HomeController");
+        }
+        [HttpGet("[action]")]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Hello, this is the Get!");
+
+            return NoContent();
+        }
+    }
+}
